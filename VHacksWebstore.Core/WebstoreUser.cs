03cdb07eb7c0bc50b040ed7d22470b6fc8b0fdc0
Tymur Arsentiev <72047302+tymur999@users.noreply.github.com>
2020-11-22 13:25:30 -0600

    removal of old project

--- VHacksWebstore.Core/WebstoreUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Application.Domain
-{
-    public class WebstoreUser : IdentityUser
-    {
-        public bool TwoFactorType { get; set; }
-    }
-}
